ede899a70152c78ddce018acaa848cc0f0b970a8
Chaitu121 <Chaitanyareddy.medapati@gmail.com>
2020-10-11 21:38:09 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
 import { Component } from '@angular/core';
-
+import { ViewChild,ElementRef } from '@angular/core'
+import { SocialAuthService } from "angularx-social-login";
+import { SocialUser } from "angularx-social-login";
+import { FacebookLoginProvider, GoogleLoginProvider } from "angularx-social-login";
+import { identifierModuleUrl } from '@angular/compiler';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+//google cloud platform
+//clent id of google 
+//872413908623-2md980ogd4dkar015ipu0env84bfuuoi.apps.googleusercontent.com
+//clent secret of google 
+//s4XxCXz6NumOpMK38T61M4dh
+//fb id  https://goldplugins.com/documentation/wp-social-pro-documentation/how-to-get-an-app-id-and-secret-key-from-facebook/
+//1134459500283856
+//secrert
+//6a9e6eb400c1fb9513ec3a011c70ea2f
+ 
+// https://www.npmjs.com/package/angularx-social-login
+
 export class AppComponent {
-  title = 'assignMterial';
+  user: SocialUser;
+  loggedIn: boolean;
+  constructor(private authService: SocialAuthService) { }
+   Name:string='Hi';
+   ngOnInit() {
+    this.authService.authState.subscribe((user) => {
+      this.user = user;
+      this.loggedIn = (user != null);
+    });
+  }
+   signInWithGoogle(): void {
+    this.authService.signIn(GoogleLoginProvider.PROVIDER_ID);
+  }
+
+  signInWithFB(): void {
+    this.authService.signIn(FacebookLoginProvider.PROVIDER_ID);
+  }
+
+  signOut(): void {
+    this.authService.signOut();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { SocialLoginModule, SocialAuthServiceConfig  } from "angularx-social-login";
+import { GoogleLoginProvider, FacebookLoginProvider } from "angularx-social-login";
+import {
+ 
+  AmazonLoginProvider,
+} from 'angularx-social-login';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+const fbLoginOptions = {
+  scope: 'pages_messaging,pages_messaging_subscriptions,email,pages_show_list,manage_pages',
+  return_scopes: true,
+  enable_profile_selector: true
+}; // https://developers.facebook.com/docs/reference/javascript/FB.login/v2.11
+
+const googleLoginOptions = {
+  scope: 'profile email'
+}; // https://developers.google.com/api-client-library/javascript/reference/referencedocs#gapiauth2clientconfig
+
+// let config = new AuthServiceConfig([
+//   {
+//     id: GoogleLoginProvider.PROVIDER_ID,
+//     provider: new GoogleLoginProvider("Google-OAuth-Client-Id", googleLoginOptions)
+//   },
+//   {
+//     id: FacebookLoginProvider.PROVIDER_ID,
+//     provider: new FacebookLoginProvider("Facebook-App-Id", fbLoginOptions)
+//   }
+// ]);
+
+
 
 @NgModule({
   declarations: [
@@ -10,9 +38,32 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, SocialLoginModule
   ],
-  providers: [],
+  providers: [ {
+    provide: 'SocialAuthServiceConfig',
+    useValue: {
+      autoLogin: false,
+      providers: [
+        {
+          id: GoogleLoginProvider.PROVIDER_ID,
+          provider: new GoogleLoginProvider(
+            '872413908623-2md980ogd4dkar015ipu0env84bfuuoi.apps.googleusercontent.com',googleLoginOptions
+          ),
+        },
+        {
+          id: FacebookLoginProvider.PROVIDER_ID,
+          provider: new FacebookLoginProvider('1134459500283856'),
+        },
+        {
+          id: AmazonLoginProvider.PROVIDER_ID,
+          provider: new AmazonLoginProvider(
+            'clientId'
+          ),
+        },
+      ],
+    } as SocialAuthServiceConfig,
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
